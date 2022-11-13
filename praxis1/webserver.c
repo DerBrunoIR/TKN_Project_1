3f7a28ce5f76eeb2050b7e149388d7d1243b84d0
DerBrunoIR <bruno.ignaz.rothschild@gmail.com>
2022-11-13 18:08:21 +0100

    added includes

--- praxis1/webserver.c
+++ praxis1/webserver.c
@@ -1,7 +1,26 @@
 #include <stdlib.h>
 
+// Beej includes
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+
+
 
 int main(int argc, char** argv) {
-    // Start here :)
-    return EXIT_SUCCESS;
+	// create socket
+	
+	// bind socket
+	
+	// wait for connection
+	
+	// receive packet
+	
+	// handle packet
+
+	// responde
+	
+
+	return EXIT_SUCCESS;
 }
