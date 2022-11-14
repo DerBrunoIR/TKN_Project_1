4c269742905f38d7703aa2cba32304dcd86a1d53
DerBrunoIR <bruno.ignaz.rothschild@gmail.com>
2022-11-14 17:22:40 +0100

    refactoring, added send

--- praxis1/webserver.c
+++ praxis1/webserver.c
@@ -10,92 +10,136 @@
 
 
 #define BACKLOG 1
+#define BUFFERSIZE 64
+#define CRLF "\n\r"
+#define log(x) printf("LOG::%s\n", x)
 
-void handle_error(int non_error_cond, char* msg) {
+void handle_error(int non_error_cond, char* msg, const char* error) {
 	if (non_error_cond != 0) {
-		fprintf(stderr, msg, strerror(errno));
+		fprintf(stderr, msg, error);
 		exit(1);
 	}
 	return;
 }
 
-// TODO: implement receiveHttp and sendHttp
-char* reveiveHttp(int port) {
-
-	return NULL;
+// for non \0 terminated strings
+int count_substring(char* s, int s_len, char* sub, int sub_len) {
+	return 1;
+	// TODO not working
+	if (s_len <= 0 || s_len < sub_len) return 0;
+	int count = 0;
+	int cur = 0;
+	for (char* c1 = s; c1 < s+s_len; s++) {
+		if (*c1 == sub[cur]) {
+			cur++;
+			if (cur == sub_len) {
+				cur = 0;
+				count++;
+			}
+		}
+	}
+	
+	return count;
 }
 
-int sendHttp(char* node, char* payload) {
-	return -1;
+int init_server_socket(char* port) {
+	int status = 0;
+
+	// getaddrinfo 
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_flags = AI_PASSIVE | AI_NUMERICSERV;
+	struct addrinfo *servinfo;
+
+	status = getaddrinfo(NULL, port, &hints, &servinfo);
+	handle_error(status, "ERROR::init_server_socket::getaddrinfo %s\n", gai_strerror(status));
+
+	// socket
+	int socket_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+	handle_error(socket_fd == -1, "ERROR::init_server_socket::socket %s\n", strerror(errno));
+
+	// bind
+	status = bind(socket_fd, servinfo->ai_addr, servinfo->ai_addrlen);
+	handle_error(status, "ERROR::init_server_socket::bind %s\n", strerror(errno));
+
+	// free
+	freeaddrinfo(servinfo);
+
+	return socket_fd;
+}
+
+int wait_and_connect(int sockfd) {
+	int status = 0;
+	// listen
+	status = listen(sockfd, BACKLOG);
+	handle_error(status, "ERROR::wait_and_connect::listen %s\n", strerror(errno));
+
+	// accept
+	log("wait_and_connect::waiting for connection...");
+	struct addrinfo coninfo;
+	int con_socket_fd = accept(sockfd, coninfo.ai_addr, &coninfo.ai_addrlen);
+	handle_error(con_socket_fd == -1, "ERROR::setup_connection::accept %s\n", strerror(errno));
+
+	return con_socket_fd;
+}
+
+char* receiveHttpPacket(int sockfd) {
+	int status = 0;
+
+	// receive packet
+	char* buffer = malloc(0);
+	int cursize = 0;
+	int remaining = 0;
+	int crlf_count = 0;
+	while (crlf_count < 3) {
+		if (remaining == 0) {
+			buffer = realloc(buffer, (cursize+BUFFERSIZE) * sizeof(char));
+			remaining = BUFFERSIZE;
+		}
+		log("receiveHttp::waiting for data");
+		status = recv(sockfd, buffer+cursize, remaining, 0);
+		handle_error(status == -1, "ERROR:receiveHttp::recv %s\n", strerror(errno));
+		remaining -= status;
+		crlf_count += count_substring(buffer+cursize, status, CRLF, 2);
+		cursize += status;
+		printf("received %d bytes\n", status);
+	}
+	cursize++;
+	buffer = realloc(buffer, cursize * sizeof(char));
+	buffer[cursize-1] = '\0';
+	
+	return buffer;
+}
+
+int sendHttpPacket(int sockfd, char* payload, int size) {
+	int status = 0;
+	int sum = 0;
+	while (sum < size) {
+		status = send(sockfd, payload, size-sum, 0);
+		handle_error(status == -1, "ERRRO::sendHttpPacket %s\n", strerror(errno));
+		sum += status;
+	}
+	return 0;
 }
 
 int main(int argc, char** argv) {
 	int status = 0;
+
 	char* port;
-	if (argc >= 2) {
-		port = argv[1]; 
-		printf("port number: %s\n", port);
-	} else {
-		printf("argument error: no port specified!\n");
-		exit(1);
-	}
+	handle_error(argc < 2, "ERROR::main::arguemnts %s\n", "Port parameter is undefined!");
+	if (argc <= 2)
+		port = argv[1];
 
-	// prepare socket creation
-	struct addrinfo hints;
-	struct addrinfo *servinfo;
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC; // select IP version automatically
-	hints.ai_socktype = SOCK_STREAM; // TCP
-	hints.ai_flags = AI_PASSIVE | AI_NUMERICSERV; // enable hoste mode, service argument is numeric string 
+	int my_sockfd = init_server_socket(port);
+	int con_sockfd = wait_and_connect(my_sockfd);
+	char* packet = receiveHttpPacket(con_sockfd);
+	printf("Data:\n%s", packet);
+	sendHttpPacket(con_sockfd, "Replay", 6);
 
-	status = getaddrinfo(NULL, port, &hints, &servinfo);
-	if (status != 0) {
-		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-		exit(1);
-	}
-	
-	// create socket
-	int socket_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-	handle_error(socket_fd == -1, "socket error: %s\n");
-	
-	// bind socket
-	status = bind(socket_fd, servinfo->ai_addr, servinfo->ai_addrlen);
-	handle_error(status, "bind error: %s\n");
-
-	// wait and acccept connection
-	status = listen(socket_fd, BACKLOG);
-	handle_error(status, "listen error: %s\n");
-
-	struct addrinfo coninfo;
-	memset(&coninfo, 0, sizeof coninfo);
-	printf("waiting for connection...\n");
-	int con_socket_fd = accept(socket_fd, coninfo.ai_addr, &coninfo.ai_addrlen);
-	handle_error(con_socket_fd == -1, "accept error: %s\n");
-
-	// receive packet
-	int sum = 0;
-	int buflen = 5;
-	char* buf = malloc((buflen+1) * sizeof(char));
-	while (sum < buflen) {
-		printf("waiting for data...\n");
-		status = recv(con_socket_fd, buf+sum, buflen-sum, 0);
-		handle_error(status == -1, "recv error: %s\n");
-		sum += status;
-	}
-
-	// handle packet
-	buf[buflen-1] = '\0';
-	printf("data:\n");
-	printf(buf);
-	
-	// respond
-	
-	
-	// free all stuff
-	free(buf);
-	close(con_socket_fd);
-	close(socket_fd);
-	freeaddrinfo(servinfo);
-
-	return EXIT_SUCCESS;
+	close(my_sockfd);
+	close(con_sockfd);
+	free(packet);
+	return 0;
 }
